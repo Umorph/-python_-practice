588cc39c877ebc1c4be9a44d57bf04b542ca6ec3
Vladislav <umorphine@gmail.com>
2020-06-04 16:39:11 +0400

    Done showfall TODO in lesson_004

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -2,13 +2,17 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 700)
 
 # На основе кода из практической части реализовать снегопад:
-# - создать списки данных для отрисовки N снежинок
+# - создать списки данных для отрисовки N снежинок (N = 20)
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+snowflake_coordinates = [
+    (100, 50),
+    (50, 100)
+]
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,12 +21,17 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
 while True:
     sd.clear_screen()
-    pass
-    pass
-    pass
+    for coordinates in snowflake_coordinates:
+        x, y = coordinates[0], coordinates[1]
+        delta_x = sd.random_number(1, 10)
+        delta_y = sd.random_number(11, 20)
+
+        x = x + delta_x
+        y = y + delta_y
+        center = sd.get_point(x, y)
+        sd.snowflake(center=center, length=50, factor_a=0.6, factor_b=0.35, factor_c=60)
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
