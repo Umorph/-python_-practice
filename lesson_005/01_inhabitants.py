aee23b451a7c1277ec32ec337cbd30779700cf3c
Vladislav <umorphine@gmail.com>
2020-06-04 16:55:49 +0400

    Done inhabitants TODO in lesson_005

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,6 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from lesson_005 import room_1
+import room_2
 
+print('В комнате room_1 живут:', room_1.folks)
+print('В комнате room_2 живут:', room_2.folks)
 
