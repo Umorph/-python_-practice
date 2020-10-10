5d43c708a381bc100c3a3988eeaf84bc7572f002
Vladislav Kantariya <umorphine@gmail.com>
2020-10-10 15:09:52 +0400

    Added first version of log parser

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -21,6 +21,44 @@
 
 # TODO здесь ваш код
 
+class LogParser:
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+
+    def parse_file(self):
+        with open(self.input_file, 'r', encoding='utf8') as log_file:
+            sorting_minute = 0
+            nok_counter = 0
+            for line in log_file:
+                current_minute = line[15:17]
+                current_status = line[29:31]
+                if current_minute == sorting_minute:
+                    if current_status == 'OK':
+                        print('current status is ok')
+                    elif current_status == 'NO':
+                        print('current status is nok')
+                        nok_counter += 1
+                    else:
+                        print('cant understand current status')
+                else:
+                    sorting_minute = current_minute
+                    with open(self.output_file, 'a', encoding='utf8') as output_file:
+                        output_file.write(str(nok_counter))
+                        output_file.write(' ')
+                        nok_counter = 0
+                    if current_status == 'OK':
+                        print('current status is ok')
+                    elif current_status == 'NO':
+                        print('current status is nok')
+                        nok_counter += 1
+                    else:
+                        print('cant understand current status')
+
+
+my_log_parser = LogParser(input_file='events.txt', output_file='output.txt')
+my_log_parser.parse_file()
+
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
